935e27308d7f08617de07124b9853ed48b6ac13a
YehorSk <95549799+YehorSk@users.noreply.github.com>
2026-05-16 19:55:46 +0200

    Initial commit

---
+++ src/main/kotlin/com/yehorsk/medicalplatformbackend/MedicalPlatformBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.yehorsk.medicalplatformbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedicalPlatformBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MedicalPlatformBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/yehorsk/medicalplatformbackend/MedicalPlatformBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yehorsk.medicalplatformbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedicalPlatformBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
